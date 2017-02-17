7bfcec3acbb448fa363b085686ae7d4a00a9cfe7
Luis Amez <Luis Amez>
2017-02-18 00:38:06 +0100

    Added marker for 'Ciudad de las Artes y las Ciencias'

--- js/script.js
+++ js/script.js
@@ -5,4 +5,10 @@ function initMap() {
         center: {lat: 39.4689262, lng: -0.3846146},
         zoom: 13
     });
+    ciudadArtes = {lat: 39.4548751, lng:-0.3526791};
+    var marker = new google.maps.Marker({
+        position: ciudadArtes,
+        map: map,
+        title: 'Ciudad de las Artes y las Ciencias'
+    });
 }
